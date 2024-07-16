9d087f771e54819a5cf8fb69d6aa2ff9a132ddc2
Harumi088 <yukinaunichi@gmail.com>
2024-07-16 16:02:03 +0800

    tiket : selamat datang

--- tiket.cpp
+++ tiket.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!";
-  cout << "Poliban nih bosa";
-  return 0;
+    int stasiunAwal, stasiunTujuan;
+
+    // Menampilkan pesan selamat datang
+    cout << "Selamat Datang di stasiun Indonesia" << endl;
+    
+    // Menampilkan informasi tarif per stasiun dan pilihan stasiun
+    cout << "Silahkan pilih tujuan anda" << endl;
+    cout << "Tarif Per-Stasiun adalah Rp5000" << endl;
+    cout << "1. Stasiun A" << endl;
+    cout << "2. Stasiun B" << endl;
+    cout << "3. Stasiun C" << endl;
+    cout << "4. Stasiun D" << endl;
+    cout << "5. Stasiun E" << endl;
+
+    cout << endl;
+    
+    cout << "Stasiun awal: ";
+    cin >> stasiunAwal;
 }
